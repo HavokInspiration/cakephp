18405d8f058f9fbf103eded0dba6be8a075fd83d
Yves <havokinspiration@gmail.com>
2015-01-31 01:20:47 +0100

    Fix more CS errors

--- tests/TestCase/Database/Schema/SqliteSchemaTest.php
+++ tests/TestCase/Database/Schema/SqliteSchemaTest.php
@@ -214,8 +214,7 @@ class SqliteSchemaTest extends TestCase
         $schema = new SchemaCollection($connection);
         $result = $schema->listTables();
 
-        if (
-            in_array($prefix . 'schema_articles', $result) &&
+        if (in_array($prefix . 'schema_articles', $result) &&
             in_array($prefix . 'schema_authors', $result)
         ) {
             return;

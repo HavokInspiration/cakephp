d6bbb7d2f692613f9d709d21b1ecd093fe8b25e2
Yves P <havokinspiration@gmail.com>
2015-02-17 21:53:46 +0100

    Another try to fix PostgreSQL build

--- tests/TestCase/DatabaseSuite.php
+++ tests/TestCase/DatabaseSuite.php
@@ -62,14 +62,15 @@ class DatabaseSuite extends TestSuite
                 ConnectionManager::get('test')->driver()->autoQuoting(false);
             },
             'Identifier Quoting / Prefix' => function () {
-                $this->prefixTestConnection();
                 foreach ($this->tests as $testsKey => $tests) {
                     foreach ($tests->tests as $testCaseKey => $testCase) {
                         if (property_exists($testCase, 'fixtureManager') && $testCase->fixtureManager instanceof FixtureManager) {
-                            $testCase->fixtureManager->resetFixture($testCase, ConnectionManager::get('testNoPrefix'));
+                            $testCase->fixtureManager->shutDown();
+                            break 2;
                         }
                     }
                 }
+                $this->prefixTestConnection();
                 ConnectionManager::get('test')->driver()->autoQuoting(true);
             },
             'No identifier quoting / Prefix' => function () {
